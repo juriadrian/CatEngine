aac6b11db3cfec8761d2c3fa60e5d2d6ee962bfe
adri <adri@adris-MacBook-Pro.local>
2019-02-25 01:21:01 -0300

    feat: initial commit

---
+++ Cat.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace catEngine
+{
+    public abstract class Cat
+    {
+        public List<string> Months;
+        public virtual bool CanHandle(string month)
+        {
+            return Months.Contains(month);
+        }
+        public abstract string Handle();
+    }
+
+    public class Persian : Cat
+    {
+        public Persian()
+        {
+            this.Months = new List<string>(){
+                "JAN", "FEB", "MAR", "APR"
+            };
+        }
+
+        public override string Handle()
+        {
+            var subtitle = "<h2>This is your kitty</h2>";
+            return subtitle + "<img border='3' src='https://vetstreet.brightspotcdn.com/dims4/default/303f54d/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F29%2Fb6%2Ff4a864144d09974dfe5bf0513e20%2FPersian-AP-V8HE5B-645sm3614.jpg'>";
+        }
+    }
+
+    public class European : Cat
+    {
+        public European()
+        {
+            this.Months = new List<string>(){
+                "MAY", "JUN", "JUL", "AUG"
+            };
+        }
+
+        public override string Handle()
+        {
+            var subtitle = "<h2>This is your kitty</h2>";
+            return subtitle + "<img border='3' src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Gatto_europeo4.jpg/250px-Gatto_europeo4.jpg'>";
+        }
+    }
+
+    public class IrishFold : Cat
+    {
+        public IrishFold()
+        {
+            this.Months = new List<string>(){
+                "SEP", "OCT", "NOV", "DIC"
+            };
+        }
+
+        public override string Handle()
+        {
+            var subtitle = "<h2>This is your kitty</h2>";
+            return subtitle + "<img border='3' src='http://www.catbreedslist.com/uploads/allimg/cat-pictures/Scottish-Fold-2.jpg'>";
+        }
+    }
+}
---
+++ catEngine.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Net;
+
+namespace catEngine
+{
+    public static class catEngine
+    {
+        public static string defaultName = "User";
+        public static string noMonthSpecified =
+            @"<h3>Please pass a month on the query string like:</h3>
+            <p>JAN | FEB | MAR | APR | MAY | JUN | JUL | AUG | SEP | OCT | NOV | DEC</p>";
+
+        [FunctionName("catEngine")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string name = req.Query["name"];
+            string month = req.Query["month"];
+
+            List<Cat> cats = new List<Cat>
+            {
+                new Persian(),
+                new European(),
+                new IrishFold()
+            };
+
+            var catInfo = cats.Find(cat => cat.CanHandle(month))?.Handle();
+
+            var source = catInfo ?? noMonthSpecified;
+
+            string html =
+                $@"<html>
+                <body style='text-align: center'>
+                <h1>Hello {name ?? defaultName}!</h1>
+                {source}
+                </body>
+                </html>";
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(html);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+    }
+}
